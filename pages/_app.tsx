3e49767090e76264b26e655c05364608096596f5
aemann2 <aemann2@gmail.com>
2021-12-08 11:32:09 -0800

    configured nextauth to use GoogleProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { SessionProvider } from 'next-auth/react';
 
-export default MyApp
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+	return (
+		<SessionProvider session={session}>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
+};
+
+export default App;
