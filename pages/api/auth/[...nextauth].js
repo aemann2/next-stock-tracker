0cd1290d63e0a1be2202a978890e5a223d2809bb
aemann2 <aemann2@gmail.com>
2021-12-08 12:37:34 -0800

    added prisma, preliminary login and test routes, set up test protected route, prisma in contact w/ db

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,17 +1,17 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-// import GithubProvider from "next-auth/providers/github"
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
-	// Configure one or more authentication providers
+	adapter: PrismaAdapter(prisma),
 	providers: [
-		// GithubProvider({
-		//   clientId: process.env.GITHUB_ID,
-		//   clientSecret: process.env.GITHUB_SECRET,
-		// }),
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		}),
 	],
+	database: process.env.DATABASE_URL,
 });
