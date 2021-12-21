b6d440e5ec1e62fc0ba718fd900cf077d5b6a3b4
aemann2 <aemann2@gmail.com>
2021-12-20 16:39:55 -0800

    added full CRUD queries to seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,33 +5,75 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient({});
 
 async function main() {
-	const john = await prisma.user.create({
+	// // CREATE
+	// const john = await prisma.user.create({
+	// 	data: {
+	// 		name: 'John Doe',
+	// 		email: 'john2.test@test.com',
+	// 	},
+	// });
+	// const stock = await prisma.stock.create({
+	// 	data: {
+	// 		userId: 'ckxb08eq300007jzt3ftpn0s7',
+	// 		symbol: 'GOOGL',
+	// 		shares: 3,
+	// 	},
+	// });
+
+	// const date = new Date();
+	// const trans = await prisma.transaction.create({
+	// 	data: {
+	// 		userId: 'ckxb08eq300007jzt3ftpn0s7',
+	// 		symbol: 'GOOGL',
+	// 		shares: 3,
+	// 		price: 421.12,
+	// 		transType: 'BUY',
+	// 		transacted: date,
+	// 	},
+	// });
+
+	// MORE CRUD:
+	// CREATE
+	const testDelete = await prisma.user.create({
 		data: {
 			name: 'John Doe',
-			email: 'john2.test@test.com',
-		},
-	});
-	const appleStock = await prisma.stock.create({
-		data: {
-			userId: 'ckxb08eq300007jzt3ftpn0s7',
-			symbol: 'AAPL',
-			shares: 2,
+			email: 'john3.test@test.com',
 		},
 	});
 
-	const date = new Date();
-	const appleTrans = await prisma.transaction.create({
+	// UPDATE
+	const updateCash = await prisma.user.update({
+		where: {
+			id: 'ckxb08eq300007jzt3ftpn0s7',
+		},
 		data: {
-			userId: 'ckxb08eq300007jzt3ftpn0s7',
-			symbol: 'AAPL',
-			shares: 2,
-			price: 1.1,
-			transType: 'BUY',
-			transacted: date,
+			balance: 8500,
 		},
 	});
 
-	// console.log({ john, appleStock, appleTrans });
+	// READ
+	const userStocks = await prisma.stock.findMany({
+		where: {
+			userId: 'ckxb08eq300007jzt3ftpn0s7',
+		},
+	});
+
+	const user = await prisma.user.findUnique({
+		where: {
+			id: 'ckxb08eq300007jzt3ftpn0s7',
+		},
+	});
+
+	// DELETE
+	const deleted = await prisma.user.delete({
+		where: {
+			email: 'john3.test@test.com',
+		},
+	});
+
+	const users = await prisma.user.findMany();
+
+	console.log({ userStocks, user, users, deleted });
 }
 
 main()
