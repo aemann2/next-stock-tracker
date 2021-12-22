ef633c512ecec5128400ca2289e66941afa9f646
aemann2 <aemann2@gmail.com>
2021-12-21 17:33:04 -0800

    began adding schema for graphql

---
+++ graphql/schema.ts
@@ -0,0 +1,39 @@
+import { gql } from 'apollo-server-micro';
+
+export const typeDefs = gql`
+	type User {
+		id: String,
+		name: String?,
+		email: String?,
+		image: String?,
+		balance: Float,
+		stocks: [Stock],
+		transactions: [Transaction]
+	}
+
+	type Stock {
+		userId: String,
+		symbol: String,
+		shares: Int
+		user: User
+	}
+
+	type Transaction {
+		userId: String,
+		symbol: String,
+		shares: Int,
+		price: Float,
+		transType: transType,
+		transacted: DateTime,
+		user: User
+	}
+
+	enum transType {
+		BUY,
+		SELL
+	}
+
+	type Query {
+		users: [User]
+	}
+`;
