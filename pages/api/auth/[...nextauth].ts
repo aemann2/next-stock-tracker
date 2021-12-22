d16726f6e0976cbbb37be893128bd6f3848b8153
aemann2 <aemann2@gmail.com>
2021-12-21 23:15:56 -0800

    added config for global prisma client

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,9 +2,7 @@ import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import prisma from '../../../lib/prisma';
 
 export default NextAuth({
 	adapter: PrismaAdapter(prisma),
