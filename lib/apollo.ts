3f8fe3e6ab0f75c72624e7034d64b885aca95c68
aemann2 <aemann2@gmail.com>
2022-01-03 10:33:07 -0800

    changed graphql uri for apollo client

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-	uri: 'http://localhost:3000/api/graphql',
+	uri: 'https://next-stock-tracker.vercel.app/api/graphql',
 	cache: new InMemoryCache(),
 });
 
