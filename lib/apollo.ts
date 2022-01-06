531a5c33c99adaf3c4423a689a965b5afe9f995b
aemann2 <aemann2@gmail.com>
2022-01-05 16:50:35 -0800

    added ssr mode to fix ssr rehydration bug

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 const apolloClient = new ApolloClient({
 	uri: 'https://next-stock-tracker.vercel.app/api/graphql',
 	cache: new InMemoryCache(),
+	ssrMode: true,
 });
 
 export default apolloClient;
