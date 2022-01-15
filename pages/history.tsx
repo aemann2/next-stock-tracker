f50a8e52057bfe85fca5188b6baca8abf57322c5
aemann2 <aemann2@gmail.com>
2022-01-14 16:58:05 -0800

    removed unused code

--- pages/history.tsx
+++ pages/history.tsx
@@ -9,11 +9,6 @@ interface IProps {
 	id: string;
 }
 
-interface stock {
-	symbol: string;
-	shares: number;
-}
-
 const TRANSACTIONS = gql`
 	query Transactions($userId: String!) {
 		transactions(userId: $userId) {
@@ -73,61 +68,3 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 		},
 	});
 };
-
-// import React from 'react';
-// import { GetServerSideProps } from 'next';
-// import { getSession } from 'next-auth/react';
-// import { gql, useQuery } from '@apollo/client';
-
-// interface IProps {
-// 	user: {
-// 		email: string;
-// 	};
-// 	userId: string;
-// }
-
-// const TRANSACTIONS = gql`
-// 	query Transactions($userId: String!) {
-// 		transactions(userId: $userId) {
-// 			symbol
-// 			shares
-// 			price
-// 			transType
-// 			transacted
-// 		}
-// 	}
-// `;
-
-// const History: React.FC<IProps> = (props) => {
-// 	const { data, error, loading } = useQuery(TRANSACTIONS, {
-// 		variables: { userId: props.userId },
-// 	});
-// 	if (loading) return <p>Loading...</p>;
-// 	if (error) return <p>Oops, something went wrong {error.message}</p>;
-// return (
-// 	<div>
-// 		<p>{props.user.email}</p>
-// 		{data.transactions.map((transaction: any, index: any) => (
-// 			<p key={index}>{transaction.symbol}</p>
-// 		))}
-// 	</div>
-// );
-// };
-
-// export default History;
-
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-// 	const session = await getSession(context);
-// 	const data = session!;
-// 	if (!session) {
-// 		return {
-// 			redirect: {
-// 				destination: '/api/auth/signin',
-// 				permanent: false,
-// 			},
-// 		};
-// 	}
-// 	return {
-// 		props: data,
-// 	};
-// };
