f303ac265c4925fe6456566b10d150dd9b851866
aemann2 <aemann2@gmail.com>
2022-01-18 13:28:33 -0800

    added logs to find error

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -39,7 +39,7 @@ const server = dev ? 'http://localhost:3000' : process.env.BASE_URL;
 console.log(dev);
 console.log(process.env.NODE_ENV);
 console.log(process.env.BASE_URL);
-console.log(server);
+console.log(process.env.EMAIL_SERVER);
 
 const Buy: React.FC<IProps> = (props) => {
 	const [BuyStock, { data: data, loading: loading, error: error }] =
